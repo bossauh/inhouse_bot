6a1719a409e5c3715bf0e359a2ca641b081ad2b1
Philippe Mathew <63218041+bossauh@users.noreply.github.com>
2022-04-04 11:27:52 +0800

    feat: add timeout_user coro

--- inhouse_bot/common_utils/__init__.py
+++ inhouse_bot/common_utils/__init__.py
@@ -1,3 +1,6 @@
+import os
+import datetime
+import aiohttp
 
 def has_discord_role(author, role) -> bool:
     roles = [x.name.upper() for x in author.roles]
@@ -5,3 +8,11 @@ def has_discord_role(author, role) -> bool:
     if not any(x.startswith(match_role) for x in roles):
         return False
     return True
+
+
+async def timeout_user(user: int, guild: int, until: int) -> None:
+    headers = {"Authorization": f"Bot {os.environ['INHOUSE_BOT_TOKEN']}"}
+    url = f"https://discord.com/api/v9/guilds/{guild}/members/{user}"
+    timeout = (datetime.datetime.utcnow() + datetime.timedelta(seconds=until)).isoformat()
+    async with aiohttp.ClientSession(raise_for_status=True).patch(url, json={"communication_disabled_until": timeout}, headers=headers) as session:
+        pass
